6801273c33430c21fc8929efa5760fc767b3d12e
Marzieh Bitaab <mbitaab@asu.edu>
2024-03-31 21:50:38 +0000

    run container fixed
    Signed-off-by: Marzieh Bitaab <mbitaab@asu.edu>

--- run_container.sh
+++ run_container.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-CONTAINER_NAME="ac"
+# /mnt/marzi/experminets/scam_collector/scam_directory/crawled/scam_dir_2024-03-29.txt
 
-CMD="\
-    python ./src/app.py \
-        --input_bp /input/buster_2024-03-29.txt \
-        --p_log_file_address /output//buster_2024-03-29.jsonl \
-        --log_file_address /logs/buster_2024-03-29.log \
-        --html_file_address /checkout_pages \
-        --screen_file_address /screenshots"
-
-
-docker run --rm --name ac_container_2 \
+docker run -d \
     -v /mnt/marzi/experminets/scam_collector/buster/crawled:/input \
     -v /mnt/marzi/experminets/scam_collector/buster/ac_result:/output \
-    -v /mnt/marzi/experminets/scam_collector/logs:/logs \
-    -v /mnt/marzi/experminets/scam_collector/checkout_pages:/checkout_pages \
-    -v /mnt/marzi/experminets/scam_collector/screenshots:/screenshots \
-    -v /mnt/marzi/experminets/scam_collector/logs:/logs \
-    --network=host \
-    --entrypoint /bin/bash ${CONTAINER_NAME} -c "${CMD}"
+    -v /mnt/marzi/experminets/scam_collector/logs:/logs/ \
+    -v /mnt/marzi/experminets/scam_collector/checkout_pages:/checkout_pages/ \
+    -v /mnt/marzi/experminets/scam_collector/screenshots:/screenshots/ \
+    --network="host"  \
+    ac \
+    python ./src/app.py \
+    --input_bp /input/scam_dir_2024-03-29.txt \
+    --p_log_file_address /output/scam_dir_2024-03-29.jsonl \
+    --log_file_address /logs/scam_dir_2024-03-29.log \
+    --html_file_address /checkout_pages/ \
+    --screen_file_address /screenshots/ \
+
